ae63296daf46be7e5b1316a0bc1b81d5b88ac720
H1lp0p <sasha.iadr@gmail.com>
2026-03-24 04:47:54 +0700

    + к прошлому

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.android.application)
 }
 
@@ -35,10 +37,23 @@ android {
     }
     buildFeatures {
         viewBinding = true
+        compose = true
     }
 }
 
+kotlin {
+    jvmToolchain(11)
+}
+
 dependencies {
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.navigation.compose)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
